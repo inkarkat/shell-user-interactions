797c0f93f352942f292e3c30b8e1871874d404bd
Ingo Karkat <swdev@ingo-karkat.de>
2020-07-14 20:50:30 +0200

    WIP: Initial API draft and first usage test

---
+++ tests/invocationNotification/fixture.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export INVOCATIONNOTIFICATION_SINK=/dev/stdout
+export INVOCATIONNOTIFICATION_SUCCESS_DISPLAY_DELAY=0
+export INVOCATIONNOTIFICATION_FAIL_DISPLAY_DELAY=0
+
+export SINGLE_LINE_COMMAND='echo from command >&2'
+export MULTI_LINE_COMMAND='{ echo from command; echo more from command; } >&2'
+
+export SAVE_CURSOR_POSITION='[s'
+export RESTORE_CURSOR_POSITION='[u'
+
+assertNoDelay()
+{
+    export INVOCATIONNOTIFICATION_SUCCESS_DISPLAY_DELAY=11
+    export INVOCATIONNOTIFICATION_FAIL_DISPLAY_DELAY=11
+    run timeout 9 "$@"
+    [ $status -ne 124 ]
+}
