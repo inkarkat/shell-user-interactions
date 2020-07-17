7eb62175f5b6ae2214795c9f301976b9fe9720e1
Ingo Karkat <swdev@ingo-karkat.de>
2020-07-17 20:16:41 +0200

    Use INVOCATIONNOTIFICATION_TITLE_{PRE,SUF}FIX and remove unused *_UPDATE_DELAY variables

--- tests/invocationNotification/title.bash
+++ tests/invocationNotification/title.bash
@@ -4,12 +4,13 @@ export INVOCATIONNOTIFICATION_SINK=/dev/stdout
 
 case "$TERM" in
     screen*)
-	readonly R=']2;'
-	readonly N='\'
+	readonly R=']2;['
+	readonly N=']\'
+	readonly C=']2;\'
 	;;
     *)
-	readonly R=']2;'
-	readonly N=''
+	readonly R=']2;['
+	readonly N=']'
+	readonly C=']2;'
 	;;
 esac
-readonly C="${R}${N}"
