6edf7cc781e642e375765d5b3e9944e0e4575385
Ingo Karkat <swdev@ingo-karkat.de>
2020-07-17 18:26:23 +0200

    WIP: Implement execution of command

--- tests/invocationNotification/command.bash
+++ tests/invocationNotification/command.bash
@@ -4,7 +4,7 @@ readonly RUNS="${BATS_TMPDIR}/runs"
 setup() {
     rm -f "$RUNS"
 }
-printf -v INVOCATIONNOTIFICATION_COMMANDLINE 'printf %%s\\\\n {} >> %q' "$RUNS"
+printf -v INVOCATIONNOTIFICATION_COMMANDLINE 'printf \\[%%s\\]\\\\n {} >> %q' "$RUNS"
 export INVOCATIONNOTIFICATION_COMMANDLINE
 
 assert_runs() {
