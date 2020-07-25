8861a71a2c1a71664971cd42fc1992dba17ece14
Ingo Karkat <swdev@ingo-karkat.de>
2020-07-25 19:22:07 +0200

    Tests: Refactoring: Define <fixture>Setup() in addition to setup()
    When multiple fixtures are load'ed, the last (inherited) setup() wins. In order to have all fixtures, run, the test itself has to define its own setup() and then invoke the fixtures' setups, which is possible due to the now unique naming. A single used fixture will still install itself without further action by delegating to <fixture>Setup() from its setup().

--- tests/invocationNotification/command.bash
+++ tests/invocationNotification/command.bash
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 readonly RUNS="${BATS_TMPDIR}/runs"
-setup() {
+commandSetup() {
     rm -f "$RUNS"
+}
+setup() {
+    commandSetup
 }
 printf -v INVOCATIONNOTIFICATION_COMMANDLINE 'printf \\[%%s\\]\\\\n {} >> %q' "$RUNS"
 export INVOCATIONNOTIFICATION_COMMANDLINE
+
+printf -v COMMANDLINE_FAIL_LATER 'echo X >> %q; [ $(cat %q | wc -l) -lt ${COMMANDLINE_FAIL_AFTER:?} ] || exit $COMMANDLINE_FAIL_AFTER' "$RUNS" "$RUNS"
+export COMMANDLINE_FAIL_AFTER
 
 assert_runs() {
     local runsContents="$(< "$RUNS")"
