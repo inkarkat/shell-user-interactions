5eed5b5a8137641524ba3e6ae05e968ee8835c22
Ingo Karkat <swdev@ingo-karkat.de>
2022-01-21 10:05:07 +0100

    Tests: Cover --to title

---
+++ tests/progressNotification/title.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+case "$TERM" in
+    screen*)
+	readonly R=']2;['
+	readonly N=']\'
+	readonly C=']2;\'
+	;;
+    *)
+	readonly R=']2;['
+	readonly N=']'
+	readonly C=']2;'
+	;;
+esac
