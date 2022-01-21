54d979fa983c206f27894ee62397d877a53f0430
Ingo Karkat <swdev@ingo-karkat.de>
2022-01-21 16:21:17 +0100

    invocationNotification: TESTS: FIX: COMMANDLINE_FAIL_AFTER must be executed in subshell so that the exit does not happen inside the main script itself
    So far, this fortunately didn't matter.

--- tests/invocationNotification/command.bash
+++ tests/invocationNotification/command.bash
@@ -10,7 +10,7 @@ setup() {
 printf -v INVOCATIONNOTIFICATION_COMMANDLINE 'printf \\[%%s\\]\\\\n {} >> %q' "$RUNS"
 export INVOCATIONNOTIFICATION_COMMANDLINE
 
-printf -v COMMANDLINE_FAIL_LATER 'echo X >> %q; [ $(cat %q | wc -l) -lt ${COMMANDLINE_FAIL_AFTER:?} ] || exit $COMMANDLINE_FAIL_AFTER' "$RUNS" "$RUNS"
+printf -v COMMANDLINE_FAIL_LATER '(echo X >> %q; [ $(cat %q | wc -l) -lt ${COMMANDLINE_FAIL_AFTER:?} ] || exit $COMMANDLINE_FAIL_AFTER)' "$RUNS" "$RUNS"
 export COMMANDLINE_FAIL_AFTER
 
 assert_runs() {
