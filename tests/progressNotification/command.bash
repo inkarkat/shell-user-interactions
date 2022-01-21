2f4cabab391a0f6eb04e91ff0f79c89ad0a1339b
Ingo Karkat <swdev@ingo-karkat.de>
2022-01-21 15:59:09 +0100

    TESTS: FIX: COMMANDLINE_FAIL_AFTER must be executed in subshell so that the exit does not happen inside the main script itself
    So far, this fortunately didn't matter.

--- tests/progressNotification/command.bash
+++ tests/progressNotification/command.bash
@@ -10,7 +10,7 @@ setup() {
 printf -v PROGRESSNOTIFICATION_COMMANDLINE 'printf \\[%%s\\]\\\\n {} >> %q' "$RUNS"
 export PROGRESSNOTIFICATION_COMMANDLINE
 
-printf -v COMMANDLINE_FAIL_LATER 'echo X >> %q; [ $(cat %q | wc -l) -lt ${COMMANDLINE_FAIL_AFTER:?} ] || exit $COMMANDLINE_FAIL_AFTER' "$RUNS" "$RUNS"
+printf -v COMMANDLINE_FAIL_LATER '(echo X >> %q; [ $(cat %q | wc -l) -lt ${COMMANDLINE_FAIL_AFTER:?} ] || exit $COMMANDLINE_FAIL_AFTER)' "$RUNS" "$RUNS"
 export COMMANDLINE_FAIL_AFTER
 
 assert_runs() {
